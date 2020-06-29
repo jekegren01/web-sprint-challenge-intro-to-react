a8ac8d74419d5acaf5f3b8981524fadb8bbfce39
Jacob Ekegren <JacobEkegren@MacBook-Pro.local>
2020-06-28 17:54:45 -0700

    Finished accessing api, built character.js. Working on Styling

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,17 +1,65 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
+import Character from './components/Character';
+import styled, { css } from 'styled-components';
+
+export const PicNameContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  `;
+
+  const Title = styled.h1`
+   text-align: center;
+   color: white;
+   font-size: 3rem;
+   -webkit-text-stroke 2px black;
+   text-stroke: 2px black;
+ `;
+
+export const PicName = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
+    const [ characters, setCharacters ] = useState([])
+
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  useEffect(() => {
+    axios
+      .get('https://rickandmortyapi.com/api/character/')
+      .then(res => {
+        console.log('Res from useEffect of Rick&Morty', res);
+        setCharacters(res.data.results);
+      })
+      .catch(err => {
+        console.log('Error from useEffect of Rick&Morty', err);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <Title>Rick and Morty Characters</Title>
+      <PicNameContainer>
+      {characters.map(character => {
+        return (
+          <Character 
+            key={character.id}
+            name={character.name}
+            image={character.image}
+          />
+        );
+      })}
+      </PicNameContainer>
     </div>
   );
 }
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,17 @@
 // Write your Character component here
+
+import React from 'react';
+import { PicNameContainer, PicName } from '../App'
+
+
+const Character = (props) => {
+    return (
+            <div>
+                <img src={props.image} alt='Pic of character'/>
+                <p>{props.name}</p>
+            </div>
+        
+    )
+}
+
+export default Character;
